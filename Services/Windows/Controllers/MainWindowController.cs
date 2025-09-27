121d64f05cfc67e37a3a8817985bf2baed5915ed
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-09-27 22:09:18 +0200

    Integrate `LogController` into `SplashScreenPageViewModel` and `MainWindowController` for enhanced logging functionality. Add `InitializeLoggerAsync` method to manage logger initialization during Splash Screen loading.

--- Services/Windows/Controllers/MainWindowController.cs
+++ Services/Windows/Controllers/MainWindowController.cs
@@ -4,10 +4,11 @@
 public sealed class MainWindowController
 {
 	private bool _initialized;
+	private readonly LogController _logController;
 
-	public MainWindowController()
+	public MainWindowController(LogController lc)
 	{
-		
+		_logController = lc;
 	}
 
 	public async Task<bool> Initialize()
@@ -18,13 +19,13 @@ public sealed class MainWindowController
 
 			if (App.MainWindow is null)
 			{
-				//_logController.Error("MainWindow not available yet. Can't initialize controller.");
+				_logController.Error("MainWindow not available yet. Can't initialize controller.");
 				return false;
 			}
 
 			if (!App.MainWindow.IsLoaded)
 			{
-				//_logController.Error("MainWindow available, but not loaded yet. Can't initialize controller.");
+				_logController.Error("MainWindow available, but not loaded yet. Can't initialize controller.");
 				return false;
 			}
 
@@ -37,15 +38,15 @@ public sealed class MainWindowController
 		}
 		catch (Exception ex)
 		{
-			//_logController.Exception(ex);
+			_logController.Exception(ex);
 			return false;
 		}
 	}
 
 	private async Task OnPositionSizeChanged(PixelPoint position, Size size)
 	{
-		//_logController.Debug($"Window Position: X={position.X}, Y={position.Y}");
-		//_logController.Debug($"Window Size: Width={size.Width}, Height={size.Height}");
+		_logController.Debug($"Window Position: X={position.X}, Y={position.Y}");
+		_logController.Debug($"Window Size: Width={size.Width}, Height={size.Height}");
 		if (!_initialized) return;
 		// Save window properties to config
 		//_configController.Config.WindowPosition = new(position.X, position.Y);
--- ViewModels/Pages/SplashScreenPageViewModel.cs
+++ ViewModels/Pages/SplashScreenPageViewModel.cs
@@ -6,11 +6,14 @@ using Services.Windows.Controllers;
 public sealed partial class SplashScreenPageViewModel : ViewModelBase
 {
 	private readonly MainWindowController _mainWindowController;
+	private readonly LogController _logController;
 	private readonly IMessenger _messenger = WeakReferenceMessenger.Default;
 
-	public SplashScreenPageViewModel(MainWindowController mwc)
+	public SplashScreenPageViewModel(MainWindowController mwc, LogController lc)
 	{
 		_mainWindowController = mwc;
+		_logController = lc;
+		_loadQueue.Enqueue(InitializeLoggerAsync);
 		_loadQueue.Enqueue(InitializeAppWindowAsync);
 		// _loadQueue.Enqueue(InitializeDebugAsync);
 		TriggerNextLoadStep();
@@ -56,6 +59,28 @@ public sealed partial class SplashScreenPageViewModel : ViewModelBase
 			}
 
 			return await _mainWindowController.Initialize();
+		}
+	}
+
+	private async Task<bool> InitializeLoggerAsync()
+	{
+		try
+		{
+			ShowInfiniteProgressBar = true;
+			LoadingText = "Initializing Logger..";
+			if (!await _logController.EnsureLogFile())
+			{
+				DisplayInfoBar("Error", "Error while trying to ensure Log-File.", InfoBarSeverity.Error);
+				return false;
+			}
+			_logController.Info("Logger initialized.");
+			ShowInfiniteProgressBar = false;
+			return true;
+		}
+		catch (Exception e)
+		{
+			DisplayInfoBar("Critical Error", $"While initializing Logger:\n{e.Message}", InfoBarSeverity.Error);
+			return false;
 		}
 	}
 
