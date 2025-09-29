4d738a77015025170be8e7b0ab52267c8fe9d7ab
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-09-29 17:03:46 +0200

    Refactor `PlannableDayListControl`: rename controls and events for private accessibility, remove `IsAddEnabled` property, and simplify selection and button handling logic.

--- Views/Controls/Lists/PlannableDayListControl.axaml.cs
+++ Views/Controls/Lists/PlannableDayListControl.axaml.cs
@@ -8,23 +8,23 @@ public class PlannableDayListControl : TemplatedControl
 		if (_addButton is not null)
 			_addButton.Click += OnAddButtonClick;
 
-		DeleteButton = e.NameScope.Find<Button>("DeleteButton");
-		if (DeleteButton is not null)
+		_deleteButton = e.NameScope.Find<Button>("DeleteButton");
+		if (_deleteButton is not null)
 		{
-			DeleteButton.Click += OnDeleteButtonClick;
-			DeleteButton.IsVisible = false;
+			_deleteButton.Click += OnDeleteButtonClick;
+			_deleteButton.IsVisible = false;
 		}
 
-		List = e.NameScope.Find<ListBox>("List");
-		if (List is not null)
-			List.SelectionChanged += SelectionChanged;
+		_list = e.NameScope.Find<ListBox>("List");
+		if (_list is not null)
+			_list.SelectionChanged += SelectionChanged;
 
 		base.OnApplyTemplate(e);
 	}
 
 	#region LIST
 
-	public ListBox? List;
+	private ListBox? _list;
 
 	public static readonly StyledProperty<ObservableCollection<DbPlannableDay>> ItemsProperty =
 		AvaloniaProperty.Register<PlannableDayListControl, ObservableCollection<DbPlannableDay>>(nameof(Items), defaultValue: []);
@@ -45,66 +45,52 @@ public class PlannableDayListControl : TemplatedControl
 
 	private Button? _addButton;
 
-	public static readonly StyledProperty<bool> IsAddEnabledProperty = AvaloniaProperty.Register<PlannableDayListControl, bool>(
-		nameof(IsAddEnabled), defaultValue: true);
-
-	public bool IsAddEnabled
-	{
-		get => GetValue(IsAddEnabledProperty);
-		set => SetValue(IsAddEnabledProperty, value);
-	}
-
-	public readonly RoutedEvent<RoutedEventArgs> AddButtonClickedEvent =
+	private readonly RoutedEvent<RoutedEventArgs> _addButtonClickedEvent =
 		RoutedEvent.Register<PlannableDayListControl, RoutedEventArgs>(nameof(PlannableDayListControl), RoutingStrategies.Bubble);
 
 	public event EventHandler<RoutedEventArgs> AddButtonClicked
 	{
-		add => AddHandler(AddButtonClickedEvent, value);
-		remove => RemoveHandler(AddButtonClickedEvent, value);
+		add => AddHandler(_addButtonClickedEvent, value);
+		remove => RemoveHandler(_addButtonClickedEvent, value);
 	}
 
 	private void OnAddButtonClick(object? sender, RoutedEventArgs e)
-	{
-		if (!IsAddEnabled) return;
-		RaiseEvent(new () { RoutedEvent = AddButtonClickedEvent});
-	}
+		=> RaiseEvent(new () { RoutedEvent = _addButtonClickedEvent});
 
 	#endregion
 
 	#region DELETE
 
-	public Button? DeleteButton;
+	private Button? _deleteButton;
 
-	public readonly RoutedEvent<RoutedEventArgs> DeleteButtonClickedEvent =
+	private readonly RoutedEvent<RoutedEventArgs> _deleteButtonClickedEvent =
 		RoutedEvent.Register<PlannableDayListControl, RoutedEventArgs>(nameof(PlannableDayListControl), RoutingStrategies.Bubble);
 
 	public event EventHandler<RoutedEventArgs> DeleteButtonClicked
 	{
-		add => AddHandler(DeleteButtonClickedEvent, value);
-		remove => RemoveHandler(DeleteButtonClickedEvent, value);
+		add => AddHandler(_deleteButtonClickedEvent, value);
+		remove => RemoveHandler(_deleteButtonClickedEvent, value);
 	}
 
 	private void OnDeleteButtonClick(object? sender, RoutedEventArgs e)
-	{
-		if (!IsAddEnabled) return;
-		RaiseEvent(new () { RoutedEvent = DeleteButtonClickedEvent});
-	}
+		=> RaiseEvent(new () { RoutedEvent = _deleteButtonClickedEvent});
 
 	#endregion
 
 	#region SELECTION
 
 	public uint SelectedPlannableDayId
-		=> List?.SelectedItem is DbPlannableDay selectedDay ? selectedDay.Id : 0;
+		=> _list?.SelectedItem is DbPlannableDay selectedDay ? selectedDay.Id : 0;
 
 	private void SelectionChanged(object? sender, SelectionChangedEventArgs e)
 		=> ToggleRemoveEnable();
 
 	private void ToggleRemoveEnable()
 	{
-		if (List is null || DeleteButton is null) return;
-		DeleteButton.IsVisible = List.Items.Count > 0;
-		DeleteButton.IsEnabled = List.SelectedIndex >= 0;
+		if (_list is null || _deleteButton is null) return;
+
+		_deleteButton.IsVisible = _list.Items.Count > 0;
+		_deleteButton.IsEnabled = _list.SelectedIndex >= 0;
 	}
 
 	#endregion
