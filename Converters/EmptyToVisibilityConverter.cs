dc57ba4f6da25c80d1b90c6a0c65496a6be87e65
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-09-29 16:54:30 +0200

    Refactor `EmptyToVisibilityConverter`: simplify `Convert` method to single-line expression.

--- Converters/EmptyToVisibilityConverter.cs
+++ Converters/EmptyToVisibilityConverter.cs
@@ -2,11 +2,7 @@
 
 public sealed class EmptyToVisibilityConverter : IValueConverter
 {
-	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-	{
-		Console.WriteLine($"EmptyToVisibilityConverter: called with count: {(value as ICollection)!.Count}");
-		return (value as ICollection)!.Count == 0;
-	}
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => (value as ICollection)!.Count == 0;
 
 	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
 }
