a760e8664c2bc50b0e07021bc471e5874be6bd4d
JumpSpinn <jumpspinn@gmx.de>
2025-09-30 10:57:50 +0200

    Add `DialogType` enum to define dialog box types in `OfficeTracker.Enums`.

---
+++ Enums/DialogType.cs
@@ -0,0 +1,14 @@
+﻿namespace OfficeTracker.Enums;
+
+/// <summary>
+/// Represents the different types of dialog boxes that can be displayed.
+/// </summary>
+public enum DialogType
+{
+	WITHOUT_ICON,
+	WARNING,
+	ERROR,
+	INFO,
+	QUESTION,
+	SUCCESS
+}
