7447c48c65b0f9c1022b7ca9601fa9105b212f5a
JumpSpinn <jumpspinn@gmx.de>
2025-09-30 15:21:52 +0200

    Update `WeekIndicatorComponent` to support templated day labels and dynamic color bindings. Adjust `CalculatedWeekListControl` to populate labels and improve reusability.

--- Components/WeekIndicatorComponent.axaml.cs
+++ Components/WeekIndicatorComponent.axaml.cs
@@ -1,6 +1,31 @@
 ﻿namespace OfficeTracker.Components;
 
+/// <summary>
+/// Represents a custom UI component that displays indicators for days of the week.
+/// </summary>
 public class WeekIndicatorComponent : TemplatedControl
 {
-}
+	/// <summary>
+	/// Identifies a styled property representing the short label for a day of the week.
+	/// </summary>
+	public static readonly StyledProperty<string> ShortWeekDayLabelProperty =
+		AvaloniaProperty.Register<PlannableDayListControl, string>(nameof(ShortWeekDayLabel), defaultValue: "-");
 
+	public string ShortWeekDayLabel
+	{
+		get => GetValue(ShortWeekDayLabelProperty);
+		set => SetValue(ShortWeekDayLabelProperty, value);
+	}
+
+	/// <summary>
+	/// Identifies a styled property representing the color configuration for a specific day type.
+	/// </summary>
+	public static readonly StyledProperty<PlannableDayColorPair> DayTypeColorProperty =
+		AvaloniaProperty.Register<PlannableDayListControl, PlannableDayColorPair>(nameof(DayType), defaultValue: new PlannableDayColorPair("#FFFFFF", "#FFFFFF"));
+
+	public PlannableDayColorPair DayTypeColor
+	{
+		get => GetValue(DayTypeColorProperty);
+		set => SetValue(DayTypeColorProperty, value);
+	}
+}
