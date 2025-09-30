abdb3ec7e4821c3f2f83f3d6c8441cc725f19a90
JumpSpinn <jumpspinn@gmx.de>
2025-09-30 15:31:51 +0200

    Add `DateTimeToDateConverter` for formatting dates and update resource dictionary with the new converter.

---
+++ Converters/DateTimeToDateConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace OfficeTracker.Converters;
+
+public class DateTimeToDateConverter : IValueConverter
+{
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is DateTime dateTime)
+			return dateTime.ToString("dd.MM.yyyy");
+		return value;
+	}
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
