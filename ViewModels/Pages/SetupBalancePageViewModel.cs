e66b547ecb03257807bc563eff7b8a37def7d8bb
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-10-01 21:38:32 +0200

    Add slider binding and XML documentation to `SetupBalancePage` view and viewmodel, implement percentage calculation logic.

--- ViewModels/Pages/SetupBalancePageViewModel.cs
+++ ViewModels/Pages/SetupBalancePageViewModel.cs
@@ -1,11 +1,39 @@
 ﻿namespace OfficeTracker.ViewModels.Pages;
 
+/// <summary>
+/// Represents the ViewModel for the Setup Balance page of the application.
+/// </summary>
 [RegisterSingleton]
 public sealed partial class SetupBalancePageViewModel : ViewModelBase
 {
-	public SetupBalancePageViewModel()
-	{
+	private readonly LogController _logController;
 
+	public SetupBalancePageViewModel(LogController lc)
+	{
+		_logController = lc;
+		HomeOfficePercentage = "50%";
+		OfficePercentage = "50%";
+		SliderValue = 50;
+	}
+
+	[ObservableProperty]
+	private string _homeOfficePercentage;
+
+	[ObservableProperty]
+	private string _officePercentage;
+
+	[ObservableProperty]
+	private double _sliderValue;
+
+	/// <summary>
+	/// Updates the home office percentage, office percentage, and slider value
+	/// based on the given slider value and its maximum value.
+	/// </summary>
+	public void CalculateBalance(double sliderValue, double sliderMaxValue)
+	{
+		HomeOfficePercentage = $"{sliderValue}%";
+		OfficePercentage = $"{sliderMaxValue - sliderValue}%";
+		SliderValue = sliderValue;
 	}
 
 	[RelayCommand]
--- Views/Pages/SetupBalancePage.axaml.cs
+++ Views/Pages/SetupBalancePage.axaml.cs
@@ -1,10 +1,23 @@
 ﻿namespace OfficeTracker.Views.Pages;
 
+/// <summary>
+/// Represents the SetupBalancePage in the OfficeTracker application.
+/// </summary>
 public partial class SetupBalancePage : UserControl
 {
 	public SetupBalancePage()
 	{
 		InitializeComponent();
+	}
+
+	/// <summary>
+	/// Handles the event triggered when the slider value is changed.
+	/// </summary>
+	private void OnBalanceSliderChanged(object? sender, RangeBaseValueChangedEventArgs e)
+	{
+		if (DataContext is not SetupBalancePageViewModel sbpvw) return;
+		if (sender is not Slider slider) return;
+		sbpvw.CalculateBalance(slider.Value, slider.Maximum);
 	}
 }
 
