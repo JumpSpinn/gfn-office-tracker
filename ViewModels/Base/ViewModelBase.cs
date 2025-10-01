096059af5d730c04e8bee0a9c246650b8822cc23
JumpSpinn <jumpspinn@gmx.de>
2025-10-01 15:12:15 +0200

    Make `ViewModelBase` abstract and refactor `ChangePage` to be a protected method with added documentation.

--- ViewModels/Base/ViewModelBase.cs
+++ ViewModels/Base/ViewModelBase.cs
@@ -3,11 +3,18 @@
 /// <summary>
 /// Represents the base class for all view models in the application.
 /// </summary>
-public class ViewModelBase : ObservableObject
+public abstract class ViewModelBase : ObservableObject
 {
+	/// <summary>
+	/// Provides access to the messaging instance used for communication between components
+	/// in the application, such as sending and receiving messages.
+	/// </summary>
 	private readonly IMessenger _messenger = WeakReferenceMessenger.Default;
 
     public ViewModelBase? PreviousPage { get; set; }
 
-    public void ChangePage(Page page) => _messenger.Send(new ChangePageMessage(page));
+    /// <summary>
+    /// Changes the current application page to the specified <paramref name="page"/>.
+    /// </summary>
+    protected void ChangePage(Page page) => _messenger.Send(new ChangePageMessage(page));
 }
