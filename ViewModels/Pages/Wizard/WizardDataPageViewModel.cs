c76b471dea94d4c0de2103b68f169ce79c8ae330
JumpSpinn <jumpspinn@gmx.de>
2025-10-02 13:17:40 +0200

    Add `CanTrackCurrentDay` property to `WizardDataPageViewModel`, update related logic, and bind `IsEnabled` state in UI

--- ViewModels/Pages/Wizard/WizardDataPageViewModel.cs
+++ ViewModels/Pages/Wizard/WizardDataPageViewModel.cs
@@ -7,13 +7,38 @@
 public sealed partial class WizardDataPageViewModel : ViewModelBase
 {
 	[ObservableProperty]
-	private decimal _homeOfficeDays;
+	[NotifyPropertyChangedFor(nameof(CanTrackCurrentDay))]
+	private decimal? _homeOfficeDays;
 
 	[ObservableProperty]
-	private decimal _officeDays;
+	[NotifyPropertyChangedFor(nameof(CanTrackCurrentDay))]
+	private decimal? _officeDays;
 
 	[ObservableProperty]
 	private bool _currentDayTracked;
+
+	/// <summary>
+	/// Gets a value indicating whether the current day can be tracked based on the specified home office and office day values.
+	/// </summary>
+	public bool CanTrackCurrentDay => HomeOfficeDays > 0 || OfficeDays > 0;
+
+	/// <summary>
+	/// Invoked when the home office days value is changed.
+	/// </summary>
+	partial void OnHomeOfficeDaysChanged(decimal? value)
+	{
+		if (value is null || (value == 0 && OfficeDays == 0))
+			CurrentDayTracked = false;
+	}
+
+	/// <summary>
+	/// Invoked when the office days value is changed.
+	/// </summary>
+	partial void OnOfficeDaysChanged(decimal? value)
+	{
+		if (value is null || (value == 0 && HomeOfficeDays == 0))
+			CurrentDayTracked = false;
+	}
 
 	[RelayCommand]
 	private void NextSetupPage() => ChangePage(Page.WIZARD_COMPLETED);
