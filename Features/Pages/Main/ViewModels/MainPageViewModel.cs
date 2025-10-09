2c1469bec77e8343f73e14b771f0d3146a62cf43
JumpSpinn <jumpspinn@gmx.de>
2025-10-09 12:55:18 +0200

    add: loading template for main page view

--- Features/Pages/Main/ViewModels/MainPageViewModel.cs
+++ Features/Pages/Main/ViewModels/MainPageViewModel.cs
@@ -20,16 +20,25 @@ public sealed partial class MainPageViewModel : ViewModelBase
 	    _calculateWeekService = cws;
     }
 
+    #region INITIALIZE
+
+    [ObservableProperty]
+    private bool _isInitializing = true;
+
     /// <summary>
     /// Asynchronously initializes the state of the MainPageViewModel by creating a new stats control
     /// and loading plannable days data.
     /// </summary>
     public async Task InitializeAsync()
     {
+	    IsInitializing = true;
 	    await ReCalculateWeeksAsync();
 	    await RefreshStatisticsAsync();
 	    await LoadPlannableDaysAsync();
+	    IsInitializing = false;
     }
+
+    #endregion
 
     /// <summary>
     /// Validates whether a selected date is valid based on specific conditions, such as being non-null,
