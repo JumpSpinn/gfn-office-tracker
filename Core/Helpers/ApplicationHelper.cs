79cb8315473efc9a035e97ddfc33de7ad0bc3d27
JumpSpinn <jumpspinn@gmx.de>
2025-10-10 14:12:36 +0200

    add: application helper

---
+++ Core/Helpers/ApplicationHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace OfficeTracker.Core.Helpers;
+
+public static class ApplicationHelper
+{
+	/// <summary>
+	/// Restarts the application
+	/// </summary>
+	public static void Restart()
+	{
+		var processMainModule = Process.GetCurrentProcess().MainModule;
+		if (processMainModule is not null)
+			Process.Start(processMainModule.FileName);
+		Environment.Exit(0);
+	}
+}
