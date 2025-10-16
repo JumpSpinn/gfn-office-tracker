3aabbb4d5de52ef43e7b9a692e9a8d86ef82d99b
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-10-16 03:07:11 +0200

    removed note

--- Infrastructure/Database/OtContext.cs
+++ Infrastructure/Database/OtContext.cs
@@ -1,14 +1,5 @@
 ﻿namespace OfficeTracker.Infrastructure.Database;
 
-/*
- * KAIWO ALLA:
- * du könntest das mit der db auch machen wie bei minty , wir speichern das in der
- * appdata unter nem ordner der minty heißt dort liegt dann alles zur exe weil sonst
- * ist es kacke wenn die leute die exe aufn desktop packen etc , man könnte im programm
- * dann oben eine "info" knopf oder nen "?" machen und damit öffnest du dann den dateipfad
- * falls du zu dem ordner willst , sollte eig ganz nice sein
- */
-
 /// <summary>
 /// Represents the database context for the OfficeTracker application,
 /// configured to manage and interact with database models and operations.
