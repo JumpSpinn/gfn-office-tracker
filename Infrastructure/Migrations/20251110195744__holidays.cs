a0da6a18c2dbe412c681a66f82e012c4d85f016c
JumpSpinn <42447303+JumpSpinn@users.noreply.github.com>
2025-11-10 23:45:33 +0100

    add: create `holidays` table and remove `IsDeleted` column from `plannable_days`

---
+++ Infrastructure/Migrations/20251110195744__holidays.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfficeTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _holidays : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "plannable_days");
+
+            migrationBuilder.CreateTable(
+                name: "holidays",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_holidays", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "holidays");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "plannable_days",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
